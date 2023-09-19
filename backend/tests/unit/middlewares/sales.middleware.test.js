f583a2215d29848d86b88c63c665013eb883be4f
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-09-18 21:07:44 -0300

    feat: more tests for sales middleware

--- backend/tests/unit/middlewares/sales.middleware.test.js
+++ backend/tests/unit/middlewares/sales.middleware.test.js
@@ -5,8 +5,11 @@ const sinonChai = require('sinon-chai');
 const { expect } = chai;
 chai.use(sinonChai);
 
-const { validateSalesProduct } = require('../../../src/middlewares/validateSales');
-// const { inputForCreateProductModel } = require('../mocks/salesFromModel');
+const { validateSalesProduct, validateSalesQuantity } = require('../../../src/middlewares/validateSales');
+const { inputForCreateProductModel } = require('../mocks/salesFromModel');
+
+// -------------------------------------------------
+// Validações de produtos
 
 describe('Validando dados de vendas - MIDDLEWARE SALES:', function () {
   afterEach(sinon.restore);
@@ -22,6 +25,7 @@ describe('Validando dados de vendas - MIDDLEWARE SALES:', function () {
 
 //     expect(next).to.have.been.calledWith();
 //   });
+
   it('Valida se o produto é preenchido na requisição', async function () {
     const request = { body: [
         {
@@ -35,17 +39,59 @@ describe('Validando dados de vendas - MIDDLEWARE SALES:', function () {
 
     validateSalesProduct(request, response);
 
+    expect(response.status).to.have.been.calledWith(400);
     expect(response.json).to.have.been.calledWith({ message: '"productId" is required' });
   });
-//   it('Valida se existe nome de produto com menos de 5 caracteres', async function () {
-//     const request = { body: { name: 'prod' } };
-//     const response = {
-//       status: sinon.stub().returnsThis(),
-//       json: sinon.stub(),
-//     };
 
-//     validateProduct(request, response);
+//   it.todo('Verifica se o produto que será vendido existe no banco de dados');
 
-//     expect(response.json).to.have.been.calledWith({ message: '"name" length must be at least 5 characters long' });
-//   });
+// -------------------------------------------------
+// Validações de quantidade de produtos
+
+  it('Verifica se todas as validações estão ok', async function () {
+    const request = { body: inputForCreateProductModel };
+    const response = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+    const next = sinon.stub().returns();
+
+    validateSalesQuantity(request, response, next);
+
+    expect(next).to.have.been.calledWith();
+  });
+
+  it('Valida se a quantidade do produto é >= 1', async function () {
+    const request = { body: [
+        {
+          productId: 1,
+          quantity: -1,
+        },
+      ] };
+    const response = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    validateSalesQuantity(request, response);
+
+    expect(response.status).to.have.been.calledWith(422);
+    expect(response.json).to.have.been.calledWith({ message: '"quantity" must be greater than or equal to 1' });
+  });
+  it('Valida se a quantidade do produto é existente', async function () {
+    const request = { body: [
+        {
+          productId: 1,
+        },
+      ] };
+    const response = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    validateSalesQuantity(request, response);
+
+    expect(response.status).to.have.been.calledWith(400);
+    expect(response.json).to.have.been.calledWith({ message: '"quantity" is required' });
+  });
 });
