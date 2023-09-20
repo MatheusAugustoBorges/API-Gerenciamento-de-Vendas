b49deb3504d63fc6d8b00ecf985e5bfb69986da5
MatheusAugustoBorges <matheus_augusto92@hotmail.com>
2023-09-20 00:30:12 -0300

    test: testing my new ssh key

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 // Body acusando undefined é por causa da falta do express.json()
 app.use(express.json());
 
-// Não remova esse endpoint, é para o avaliador funcionar
+// Não remova esse endpoint, é para o avaliador funcionar!
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
 });
